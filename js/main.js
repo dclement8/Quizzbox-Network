ed37d39edd5e81e8193eb09918924d4d9180c35f
Thibaut Dalichampt <thibaut.dalichampt@outlook.com>
2017-03-13 14:42:43 +0100

    Modifications boutons
    Ajout question et création quizz.

--- js/main.js
+++ js/main.js
@@ -1,25 +1,25 @@
-var htmlEntities = function(str) {
-	return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
+var htmlEntities = function (str) {
+    return String(str).replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;');
 };
 
-var isNumeric = function(n) {
-	return !isNaN(parseFloat(n)) && isFinite(n);
+var isNumeric = function (n) {
+    return !isNaN(parseFloat(n)) && isFinite(n);
 };
 
-var storageAvailable = function(type) {
-	try {
-		var storage = window[type],
-			x = '__storage_test__';
-		storage.setItem(x, x);
-		storage.removeItem(x);
-		return true;
-	}
-	catch(e) {
-		return false;
-	}
+var storageAvailable = function (type) {
+    try {
+        var storage = window[type],
+            x = '__storage_test__';
+        storage.setItem(x, x);
+        storage.removeItem(x);
+        return true;
+    }
+    catch (e) {
+        return false;
+    }
 };
 
-var quizz = (function() {
+var quizz = (function () {
     // Functions nécessaires à la création/modification d'un quizz
 
     var $quizz = document.querySelector("#questions");
@@ -30,16 +30,16 @@ var quizz = (function() {
             "id_categorie": 0,
             "id": 0,
             "tokenWeb": "0",
-			"questions": [
-	            {
-	                "id": 0,
-	                "enonce": "",
-	                "coefficient": 1,
-	                "reponses": [
-	                    {"id": 0, "nom": "", "estSolution": 0}
-	                ]
-	            }
-	        ]
+            "questions": [
+                {
+                    "id": 0,
+                    "enonce": "",
+                    "coefficient": 1,
+                    "reponses": [
+                        { "id": 0, "nom": "", "estSolution": 0 }
+                    ]
+                }
+            ]
         }
     };
 
@@ -79,112 +79,112 @@ var quizz = (function() {
     */
 
     return {
-        getJSON: function() {
+        getJSON: function () {
             return json;
         },
 
-        ajouterQuestion: function() {
-            json.quizz.questions.push({"id":0,"enonce":"","coefficient":1,"reponses":[{"id":0,"nom": "", "estSolution": false}]});
-			quizz.updateStorage();
+        ajouterQuestion: function () {
+            json.quizz.questions.push({ "id": 0, "enonce": "", "coefficient": 1, "reponses": [{ "id": 0, "nom": "", "estSolution": false }] });
+            quizz.updateStorage();
             quizz.generer();
         },
 
-        ajouterReponse: function(question) {
-            json.quizz.questions[question-1].reponses.push({"id":0,"nom":"","estSolution":false});
-			quizz.updateStorage();
+        ajouterReponse: function (question) {
+            json.quizz.questions[question - 1].reponses.push({ "id": 0, "nom": "", "estSolution": false });
+            quizz.updateStorage();
             quizz.generer();
         },
 
-        supprimerQuestion: function(question) {
-            json.quizz.questions.splice(question-1, 1);
-			quizz.updateStorage();
+        supprimerQuestion: function (question) {
+            json.quizz.questions.splice(question - 1, 1);
+            quizz.updateStorage();
             quizz.generer();
         },
 
-        supprimerReponse: function(question, reponse) {
-            json.quizz.questions[question-1].reponses.splice(reponse-1, 1);
-			quizz.updateStorage();
+        supprimerReponse: function (question, reponse) {
+            json.quizz.questions[question - 1].reponses.splice(reponse - 1, 1);
+            quizz.updateStorage();
             quizz.generer();
         },
 
-        updateNom: function(value) {
+        updateNom: function (value) {
             json.quizz.nom = value;
         },
 
-        updateCategorie: function(value) {
+        updateCategorie: function (value) {
             json.quizz.id_categorie = value;
         },
 
-        updateEnonce: function(question, value) {
-            json.quizz.questions[question-1].enonce = value;
+        updateEnonce: function (question, value) {
+            json.quizz.questions[question - 1].enonce = value;
         },
 
-        updateCoefficient: function(question, value) {
-			value = parseInt(value);
-			if(isNumeric(value)) {
-            	json.quizz.questions[question-1].coefficient = value;
-			}
+        updateCoefficient: function (question, value) {
+            value = parseInt(value);
+            if (isNumeric(value)) {
+                json.quizz.questions[question - 1].coefficient = value;
+            }
         },
 
-        updateReponse: function(question, reponse, value) {
-            json.quizz.questions[question-1].reponses[reponse-1].nom = value;
+        updateReponse: function (question, reponse, value) {
+            json.quizz.questions[question - 1].reponses[reponse - 1].nom = value;
         },
 
-        updateSolution: function(question, reponse) {
-            json.quizz.questions[question-1].reponses[reponse-1].estSolution = !(json.quizz.questions[question-1].reponses[reponse-1].estSolution);
-			quizz.updateStorage();
-		},
+        updateSolution: function (question, reponse) {
+            json.quizz.questions[question - 1].reponses[reponse - 1].estSolution = !(json.quizz.questions[question - 1].reponses[reponse - 1].estSolution);
+            quizz.updateStorage();
+        },
 
-        verifierContenu: function() {
+        verifierContenu: function () {
             Quizzmsg.innerHTML = '';
             var k;
-            for(var i=0; i < json.quizz.questions.length; i++) {
-                if(json.quizz.questions[i].enonce == '' || json.quizz.questions[i].enonce === undefined) {
-                    Quizzmsg.innerHTML += 'L\'énoncé de la question '+ (i+1) +' est vide.<br />';
+            for (var i = 0; i < json.quizz.questions.length; i++) {
+                if (json.quizz.questions[i].enonce == '' || json.quizz.questions[i].enonce === undefined) {
+                    Quizzmsg.innerHTML += 'L\'énoncé de la question ' + (i + 1) + ' est vide.<br />';
                     continue;
                 }
-                if(json.quizz.questions[i].reponses.length === undefined) {
-                    Quizzmsg.innerHTML += 'La question '+ (i+1) +' doit comporter des réponses.<br />';
+                if (json.quizz.questions[i].reponses.length === undefined) {
+                    Quizzmsg.innerHTML += 'La question ' + (i + 1) + ' doit comporter des réponses.<br />';
                     continue;
                 }
                 k = false;
-                for(var j=0; j < json.quizz.questions[i].reponses.length; j++) {
-                    if(json.quizz.questions[i].reponses[j].nom == '' || json.quizz.questions[i].reponses[j].nom === undefined) {
-                        Quizzmsg.innerHTML += 'La réponse '+ (j+1) +' de la question '+ (i+1) +' est vide.<br />';
+                for (var j = 0; j < json.quizz.questions[i].reponses.length; j++) {
+                    if (json.quizz.questions[i].reponses[j].nom == '' || json.quizz.questions[i].reponses[j].nom === undefined) {
+                        Quizzmsg.innerHTML += 'La réponse ' + (j + 1) + ' de la question ' + (i + 1) + ' est vide.<br />';
                         continue;
                     }
-                    if(json.quizz.questions[i].reponses[j].estSolution == 1) {
+                    if (json.quizz.questions[i].reponses[j].estSolution == 1) {
                         k = true;
                     }
                 }
-                if(json.quizz.questions[i].reponses.length < 2) {
-                    Quizzmsg.innerHTML += 'La question '+ (i+1) +' doit comporter au moins 2 réponses.<br />';
+                if (json.quizz.questions[i].reponses.length < 2) {
+                    Quizzmsg.innerHTML += 'La question ' + (i + 1) + ' doit comporter au moins 2 réponses.<br />';
                     continue;
                 }
-                if(k === false) {
-                    Quizzmsg.innerHTML += 'La question '+ (i+1) +' doit comporter au moins une réponse juste.<br />';
+                if (k === false) {
+                    Quizzmsg.innerHTML += 'La question ' + (i + 1) + ' doit comporter au moins une réponse juste.<br />';
                     continue;
                 }
             }
-            if(Quizzmsg.innerHTML == '') {
+            if (Quizzmsg.innerHTML == '') {
                 return true;
             }
             return false;
         },
 
-		updateStorage: function() {
-			if(storageAvailable('localStorage')) {
-				console.log("update storage");
-				localStorage.setItem("quizzboxEdition", JSON.stringify(json));
-			}
-		},
+        updateStorage: function () {
+            if (storageAvailable('localStorage')) {
+                console.log("update storage");
+                localStorage.setItem("quizzboxEdition", JSON.stringify(json));
+            }
+        },
 
-        generer: function(data = null) {
+        generer: function (data = null) {
             // Génère le formulaire de questions/réponses à partir du JSON
             var tab = '';
 
-            if(data !== null) {
-				console.log(data);
+            if (data !== null) {
+                console.log(data);
                 json = data;
             }
 
@@ -193,23 +193,25 @@ var quizz = (function() {
             document.querySelector("#nom").value = json.quizz.nom;
             document.querySelector("#categorie").value = json.quizz.id_categorie;
 
-            for(var i=1; i <= json.quizz.questions.length; i++) {
-                tab += '<table id="question_'+i+'">';
-                tab += '<tr><td>Question n°'+i+' : <input type="text" onchange="quizz.updateEnonce('+i+', this.value)" onblur="quizz.updateStorage()" onkeyup="quizz.updateEnonce('+i+', this.value)" placeholder="Votre question ?" value="'+json.quizz.questions[i - 1].enonce+'" /></td>';
-                tab += '<td>coefficient <input type="number" min="1" max="5" onblur="quizz.updateStorage()" onchange="quizz.updateCoefficient('+i+', this.value)" onkeyup="quizz.updateCoefficient('+i+', this.value)" value="'+json.quizz.questions[i - 1].coefficient+'" /></td></tr>';
+            for (var i = 1; i <= json.quizz.questions.length; i++) {
+                tab += '<table id="question_' + i + '">';
+                tab += '<tr><td>Question n°' + i + ' : <input type="text" onchange="quizz.updateEnonce(' + i + ', this.value)" onblur="quizz.updateStorage()" onkeyup="quizz.updateEnonce(' + i + ', this.value)" placeholder="Votre question ?" value="' + json.quizz.questions[i - 1].enonce + '" /></td>';
+                tab += '<td>coefficient <input type="number" min="1" max="5" onblur="quizz.updateStorage()" onchange="quizz.updateCoefficient(' + i + ', this.value)" onkeyup="quizz.updateCoefficient(' + i + ', this.value)" value="' + json.quizz.questions[i - 1].coefficient + '" /></td></tr>';
 
                 // Réponses
-                for(var j=1; j <= json.quizz.questions[i - 1].reponses.length; j++) {
-                    tab += '<tr id="reponse_'+i+'_'+j+'"><td>Réponse n°'+j+' : <input type="text" placeholder="Une réponse" onblur="quizz.updateStorage()" onchange="quizz.updateReponse('+i+', '+j+', this.value)" onkeyup="quizz.updateReponse('+i+', '+j+', this.value)" value="'+json.quizz.questions[i - 1].reponses[j - 1].nom+'" /></td>';
-                    tab += '<td>est solution ? <input type="checkbox" onclick="quizz.updateSolution('+i+', '+j+')"';
-                    if(json.quizz.questions[i-1].reponses[j-1].estSolution) tab += ' checked';
+                for (var j = 1; j <= json.quizz.questions[i - 1].reponses.length; j++) {
+                    tab += '<tr id="reponse_' + i + '_' + j + '"><td>Réponse n°' + j + ' : <input type="text" placeholder="Une réponse" onblur="quizz.updateStorage()" onchange="quizz.updateReponse(' + i + ', ' + j + ', this.value)" onkeyup="quizz.updateReponse(' + i + ', ' + j + ', this.value)" value="' + json.quizz.questions[i - 1].reponses[j - 1].nom + '" /></td>';
+                    tab += '<td>est solution ? <input type="checkbox" onclick="quizz.updateSolution(' + i + ', ' + j + ')"';
+                    if (json.quizz.questions[i - 1].reponses[j - 1].estSolution) tab += ' checked';
                     tab += ' />';
-                    if(j > 1) tab += '<input type="button" value="X" onclick="quizz.supprimerReponse('+i+', '+j+')">';
+                    if (j > 1) tab += '<input type="button" value="X" onclick="quizz.supprimerReponse(' + i + ', ' + j + ')">';
                     tab += '</td></tr>';
                 }
 
-                tab += '<tr class="button"><td><input type="button" value="Ajouter une réponse" onclick="quizz.ajouterReponse('+i+')" /> </td>';
-                if(i > 1) tab += '<td><input type="button" value="Supprimer la question" onclick="quizz.supprimerQuestion('+i+')"></td>';
+                tab += '<tr class="button"><td><input type="button" value="Ajouter une réponse" onclick="quizz.ajouterReponse(' + i + ')" /> </td>';
+                tab += '<tr class="button"><td><input type="button" value="Ajouter une question" onclick="quizz.ajouterQuestion()" /> <input type="button" value="Créez le quizz" onclick="quizz.envoyer()" /></td>';
+
+                if (i > 1) tab += '<td><input type="button" value="Supprimer la question" onclick="quizz.supprimerQuestion(' + i + ')"></td>';
                 tab += '</tr>';
                 tab += '</table>';
             }
@@ -218,53 +220,53 @@ var quizz = (function() {
             console.log(json);
         },
 
-        envoyer: function() {
-            if((json.quizz.questions[0].enonce == '' || json.quizz.questions[0].enonce === undefined) ||
+        envoyer: function () {
+            if ((json.quizz.questions[0].enonce == '' || json.quizz.questions[0].enonce === undefined) ||
                 (json.quizz.questions[0].reponses[0] == '' || json.quizz.questions[0].reponses[0] === undefined) ||
                 (json.quizz.questions[0].reponses[1] == '' || json.quizz.questions[0].reponses[1] === undefined)) {
                 alert('Votre quizz doit comporter au moins 1 question et 2 réponses');
             }
-            else if(json.quizz.id_categorie == '' || json.quizz.id_categorie == 0 || json.quizz.id_categorie === undefined) {
+            else if (json.quizz.id_categorie == '' || json.quizz.id_categorie == 0 || json.quizz.id_categorie === undefined) {
                 alert('Veuillez choisir une catégorie !');
             }
-            else if(!(quizz.verifierContenu())) {
+            else if (!(quizz.verifierContenu())) {
                 alert('Votre quizz comporte des erreurs !');
             }
             else {
                 document.querySelector("#json").value = JSON.stringify(quizz.getJSON());
 
-                if(confirm("Voulez-vous valider les modifications de ce quizz ?")) {
+                if (confirm("Voulez-vous valider les modifications de ce quizz ?")) {
                     document.querySelector("#formulaire").submit();
                 }
             }
         }
     }
-}) ();
+})();
 
-var getLocal = (function() {
-	var local = null;
-	return {
-		show: function() {
-			/* Affichage bouton pour charger ou supprimer au chargement */
-			if(storageAvailable('localStorage')) {
-				var dlocal = localStorage.getItem("quizzboxEdition");
-				if(dlocal !== undefined && dlocal !== null && dlocal != '{}') {
-					local = JSON.parse(dlocal);
-					document.querySelector("#localQuizz").innerHTML = '<b>Charger un quizz en cours d\'édition</b>\
-					'+local.quizz.nom+' <input type="button" value="Charger" onclick="getLocal.load();">\
+var getLocal = (function () {
+    var local = null;
+    return {
+        show: function () {
+            /* Affichage bouton pour charger ou supprimer au chargement */
+            if (storageAvailable('localStorage')) {
+                var dlocal = localStorage.getItem("quizzboxEdition");
+                if (dlocal !== undefined && dlocal !== null && dlocal != '{}') {
+                    local = JSON.parse(dlocal);
+                    document.querySelector("#localQuizz").innerHTML = '<b>Charger un quizz en cours d\'édition</b>\
+					'+ local.quizz.nom + ' <input type="button" value="Charger" onclick="getLocal.load();">\
 					<input type="button" value="Supprimer le quizz sauvé" onclick="getLocal.delete();">';
-				}
-			}
-		},
+                }
+            }
+        },
 
-		load: function() {
-			quizz.generer(local);
-			document.querySelector("#Quizzmsg").innerHTML = 'Quizz chargé !';
-		},
+        load: function () {
+            quizz.generer(local);
+            document.querySelector("#Quizzmsg").innerHTML = 'Quizz chargé !';
+        },
 
-		delete: function() {
-			localStorage.removeItem("quizzboxEdition");
-			document.querySelector("#localQuizz").innerHTML = '';
-		}
-	}
-}) ();
+        delete: function () {
+            localStorage.removeItem("quizzboxEdition");
+            document.querySelector("#localQuizz").innerHTML = '';
+        }
+    }
+})();
